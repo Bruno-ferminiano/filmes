2a58a1d0905300d7c0a9f4d296df64f02165b3d4
Bruno-ferminiano <125706925+Bruno-ferminiano@users.noreply.github.com>
2025-08-20 11:23:29 +0000

    teste 45

--- Script.js
+++ Script.js
@@ -17,24 +17,6 @@ if (formulario) {
 }
 
 
-const loginForm = document.getElementById('loginForm');
-
-if (loginForm) {
-  loginForm.addEventListener('submit', function(event) {
-      event.preventDefault(); 
-
-      const nome = document.getElementById('nome').value;
-      const email = document.getElementById('email').value;
-      const senha = document.getElementById('senha').value;
-
-      if (nome && email && senha) {
-          alert('Login feito com sucesso, redirecionando...');
-          window.location.href = 'home.html'; 
-      } else {
-          alert('Por favor, preencha todos os campos.');
-      }
-  });
-}
 
 function minhaFuncao(){
     
@@ -43,9 +25,9 @@ const loginForm = document.getElementById('loginForm');
 if (loginForm) {
   
 
-      const nome = document.getElementById('nome').value;
-      const email = document.getElementById('email').value;
-      const senha = document.getElementById('senha').value;
+      const nome = document.getElementById('nome1').value;
+      const email = document.getElementById('email1').value;
+      const senha = document.getElementById('senha1').value;
 
       if (nome && email && senha) {
           alert('Login feito com sucesso, redirecionando...');
